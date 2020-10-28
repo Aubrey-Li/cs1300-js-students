279db8951b1a5fcea851966be65c9b63f0be2a82
Aubrey-Li <63608597+Aubrey-Li@users.noreply.github.com>
2020-10-27 22:22:08 -0400

    updates

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=y-8opUVcqOFM1BCk_aMaqN7mZ0IpUxm90jmVoRwQbFE";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -24,8 +24,29 @@ const corsPromise = () =>
 corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      let data = JSON.parse(request.response);
+        console.log(data);
+        let addCard = "";
+        data.data.forEach(function (plant) {
+            console.log(plant);
+            addCard += `<div class="plant">
+                      <h1>${plant.common_name}</h1>
+                      <p>Common Name: ${plant.family_common_name}</p>
+                      <p>Scientific Name:${plant.scientific_name}</p>
+                      <p>Year:${plant.year}</p>
+                      <p>Family: ${plant.family}</p>
+                      <p>Genus:${plant.genus}</p>
+                      <p>Bibliography: ${plant.bibliography}</p>
+                      <div class="image">
+                      <img style="width: 20%;" src="${plant.image_url}" alt="plants" class="center">
+                      <br>
+                      </div>
+                    </div>`;
+        });
+        document.getElementById("plant").innerHTML = addCard;
+        img.style.margin = "0 auto";
     })
 );
 
-//// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+
+
